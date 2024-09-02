a12fcdb1fd0c004d6101cdc11e4cbeb8b213af8a
DELL <DELL@DESKTOP-FN7PO88>
2024-09-02 03:17:20 -0700

    pushes

--- src/test/java/com/comcast/crm/orgtest/CreateOrgTest.java
+++ src/test/java/com/comcast/crm/orgtest/CreateOrgTest.java
@@ -160,7 +160,7 @@ public class CreateOrgTest extends BaseClass {
 		
 		UtilityClassObject.getTest().log(Status.INFO, "verify the details");
 		String actphone = oi.getPhoneText().getText().trim();
-		System.out.println(" close git ");
+		System.out.println(" close git ......................");
 		SoftAssert sa = new SoftAssert();
 		sa.assertEquals(actphone, phone);
 		sa.assertAll();
